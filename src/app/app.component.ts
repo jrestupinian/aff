2e7401e0c5c9384f31737b93a0a48c258bcdfb9d
Jose Roberto Estupinian <jrestupinian@gmail.com>
2019-02-28 22:53:31 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  title = 'aff';
+  projects = [{name: 'Auditoría Tarifas'}];
+
+  constructor(private api:ApiService){
+    this.getProjects();
+  }
+
+  getProjects = () => {
+    this.api.getAllProjects().subscribe(
+      data => {
+        this.projects = data;
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
 }
